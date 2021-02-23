fe25b1443f497c43ba4c2e774b3e440175926dc4
labsesdt <labsesdt@gmail.com>
2021-02-23 17:58:38 +0800

    Signed-off-by: labsesdt <labsesdt@gmail.com>

---
+++ 108590025_HW1/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        String TAG = MainActivity.class.getSimpleName();
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "Hello World");
+
+    }
+}
