b3813cc9a6cd389d57e3b2cc86c1ca354b8da740
jerry12122 <a0975844268@gmail.com>
2021-04-04 21:58:02 +0800

    HW5

---
+++ Test/app/src/main/java/com/example/test/Calculator.java
@@ -0,0 +1,43 @@
+package com.example.test;
+import java.lang.Math;
+public class Calculator {
+
+    // Available operations
+    public enum Operator {ADD, SUB, DIV, MUL, POW}
+
+    /**
+     * Addition operation
+     */
+    public double add(double firstOperand, double secondOperand) {
+        return firstOperand + secondOperand;
+    }
+
+    /**
+     * Subtract operation
+     */
+    public double sub(double firstOperand, double secondOperand) {
+        return firstOperand - secondOperand;
+    }
+
+    /**
+     * Divide operation
+     */
+    public double div(double firstOperand, double secondOperand) {
+        if (secondOperand == 0) {
+            throw new IllegalArgumentException("You cannot divide by zero");
+        }
+        return firstOperand / secondOperand;
+    }
+
+    /**
+     * Multiply operation
+     */
+    public double mul(double firstOperand, double secondOperand) {
+        return firstOperand * secondOperand;
+    }
+
+    public double pow(double firstOperand, double secondOperand) {
+        return Math.pow(firstOperand, secondOperand);
+    }
+
+}
